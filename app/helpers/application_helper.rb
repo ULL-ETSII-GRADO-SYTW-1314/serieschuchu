0ca816d108dc3770430a42023b8b2b1d47e41fca
migueladan <alu0100356137@ull.edu.es>
2013-11-12 22:09:06 +0000

    truquillo gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,11 @@ module ApplicationHelper
     def devise_mapping
       @devise_mapping ||= Devise.mappings[:user]
     end
+    
+    #helper de gravatar
+    def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png"
+    end
+
 end
