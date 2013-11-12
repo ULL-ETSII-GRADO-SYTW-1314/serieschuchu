32a81a816868b63fb3128c9033165fc539eb0507
migueladan <alu0100356137@ull.edu.es>
2013-11-12 18:14:33 +0000

    Migraciones: estado y admin en la tabla ususarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111222938) do
+ActiveRecord::Schema.define(version: 20131112180701) do
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",               default: "", null: false
+    t.string   "username",               default: "",    null: false
+    t.boolean  "admin",                  default: false
+    t.integer  "estado",                 default: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
