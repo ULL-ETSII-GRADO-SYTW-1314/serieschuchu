211349b3809f13933852118c176189e9747e018c
migueladan <alu0100356137@ull.edu.es>
2013-11-18 19:22:50 +0000

    Arreglillos visuales

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -70,6 +70,6 @@ class SeriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def series_params
-      params.require(:series).permit(:nombre, :informacion, :sinopsis, :puntuacion, :votos, :imagen, :enlace_imagen, :directores, :titulo_original)
+      params.require(:series).permit(:nombre, :informacion, :sinopsis, :puntuacion, :votos, :imagen, :enlace_imagen, :directores, :titulo_original,:generos)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118170621) do
+ActiveRecord::Schema.define(version: 20131118185416) do
 
   create_table "episodes", force: true do |t|
     t.string   "nombre"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20131118170621) do
     t.string   "titulo_original"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "generos"
   end
 
   create_table "users", force: true do |t|
