0028a525a32d5382760664c759f43a1334a01846
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-01 23:36:17 +0000

    Incluir DSL de Capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Incluir el DSL de Capybara
+  config.include Capybara::DSL
 end
