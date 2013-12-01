135b8bc995efea188599a4a6ddd1979991c0cf53
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-01 23:45:40 +0000

    testear la opción de login

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe "Welcome" do
 
   describe "Home page" do
+
     it "Debería contener una opción para registrarse" do
       visit '/'
-      expect(page).to have_content('REGISTRO')
+      expect(page).to have_content('REGISTRAR')
+    end
+   
+    it "Debería contener una opción para loguearse" do
+      visit '/'
+      expect(page).to have_content('LOGIN')
     end
   end
 end
