ae59998dd96b92d753a7b8c05867323a950da1f9
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-02 01:48:45 +0000

    Refactorizar tests

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe "Welcome" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "Debería contener una opción para registrarse" do
-      visit root_path   # visit '/'
-      expect(page).to have_content('REGISTRAR')
-    end
-   
-    it "Debería contener una opción para loguearse" do
-      visit root_path   # visit '/'
-      expect(page).to have_content('LOGIN')
-    end
-
-    it "Debería tener un título base" do
-      visit root_path   # visit '/'
-      expect(page).to have_title('Serieschuchu')
-    end
+    it { should have_content('REGISTRAR')}
+    it { should have_content('LOGIN')}
+    it { should have_title('Serieschuchu')}
   end
 end
