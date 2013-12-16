df2d765fb7682ffa4031cd28e60a439205d1fdf5
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-16 23:31:43 +0000

    Migrar base de datos con el modelo de comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118185416) do
+ActiveRecord::Schema.define(version: 20131216232633) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.boolean  "state"
+    t.integer  "user_id"
+    t.integer  "series_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["series_id"], name: "index_comments_on_series_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "episodes", force: true do |t|
     t.string   "nombre"
