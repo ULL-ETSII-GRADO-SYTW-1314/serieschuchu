cc4f220e497102d8344cb97d941700d882ee6667
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-17 00:59:15 +0000

    Definir acción create para los comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+  layout "inicio"
+  def create
+    @series = Series.find(params[:series_id])
+    parametros = params[:comment].permit(:user, :content)
+    @comment = @series.comments.create(parametros)
+    redirect_to "#{series_path(@series)}/comments"
+  end
 end
