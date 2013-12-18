b739464c77cf2d9819203d38356639f6139a0760
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-18 03:45:25 +0000

    Añadir búsqueda de usuario

--- app/controllers/listadeusuarios_controller.rb
+++ app/controllers/listadeusuarios_controller.rb
@@ -1,5 +1,11 @@
 class ListadeusuariosController < ApplicationController
   layout "inicio"
   def index
+    if params[:search]
+      @users = User.find(:all, :conditions => ['username LIKE ?',
+                                                "%#{params[:search]}%"])
+    else
+      @users = User.find(:all)
+    end
   end
 end
