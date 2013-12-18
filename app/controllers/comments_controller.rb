93072e67565102e31639273623aa3bc5bdbf5659
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-18 20:00:41 +0000

    Eliminar modelo de comentarios debido a error e implementar otro

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 class CommentsController < ApplicationController
   layout "inicio"
   def create
+    # Necesito en params[:comment]
+
     @series = Series.find(params[:series_id])
-    parametros = params[:comment].permit(:user, :content)
+    parametros = params[:comment].permit(:content, :state, :user_id)
+
     @comment = @series.comments.create(parametros)
     redirect_to "#{series_path(@series)}/comments"
+  end
+
+  def destroy
   end
 end
