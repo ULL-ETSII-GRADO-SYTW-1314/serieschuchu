3904b6d9258febaa3b7ff43040263f80b015feac
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-18 02:22:54 +0000

    Añadir método de búsqueda en el modelo

--- app/models/series.rb
+++ app/models/series.rb
@@ -2,4 +2,12 @@ class Series < ActiveRecord::Base
 	has_many :episodes
 	has_many :follow_shows
   has_many :comments
+
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['nombre LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
