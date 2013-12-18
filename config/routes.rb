2701160d9d5b82cd530d151286754add5b115e27
David Hernández Bethencourt <a1ddhernandez@gmail.com>
2013-12-18 03:44:03 +0000

    Eliminar ruta inservible

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Serieschuchu::Application.routes.draw do
   resources :series
   resources :misseries
   resources :listadeusuarios
-  match "/search" => "search#search", :as => :search, :via => :get
   get "inicio/portada"
   get "inicio/index"
   get "welcome/index"
